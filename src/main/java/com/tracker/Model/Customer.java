15b8f0785a852932777441f1a0f26cded09b2f91
AbdessadekHm <abdessadekhm@gmail.com>
2024-12-12 22:50:35 +0100

    Add Models

---
+++ src/main/java/com/tracker/Model/Customer.java
@@ -0,0 +1,14 @@
+package com.tracker.Model;
+
+public class Customer {
+    private int id;
+    private String nom, email, phone;
+
+    public Customer(int id, String nom, String email, String phone){
+        this.id = id;
+        this.nom = nom;
+        this.email = email;
+        this.phone = phone;
+    }
+    
+}
---
+++ src/main/java/com/tracker/Model/Order.java
@@ -0,0 +1,15 @@
+package com.tracker.Model;
+
+public class Order {
+    private int id, amount;
+    private String data, status;
+
+    public Order(int id, int amount, String data, String status){
+        this.id = id;
+        this.amount = amount;
+        this.data = data;
+        this.status = status;
+    }
+    
+    
+}
